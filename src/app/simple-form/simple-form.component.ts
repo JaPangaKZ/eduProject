d53730a6efbae8bd54541444c2598d3fd87020a6
JaPangaKZ <japandakz@gmail.com>
2017-09-08 18:56:59 +0600

    2

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simple-form Works!
-    </p>
+<div> 
+Write your name:<input  type="text" name="title" ng-Model="name"><br>
+Choose your birthdate:<input type="date" ng-model="date"><br>
+ImbicilMetr:<input type="range" ng-model="rangeNumber" min="0" max="100"><hr>
+{{name}}
+<button (click)="onClick()">ClickMePlease</button>
+</div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+  
+  onClick(){
+    console.log
+    //value = <code>{{name}}</code><br/>
+  }
 
   constructor() { }
 
